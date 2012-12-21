c746e3e1d727176fd484a77ed30ab98ef7c561d6
bdesgraupes <bdesgraupes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-21 08:51:31 +0000

    Transposed robespierre
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lexicalStat/data/robespierre.R
+++ pkg/lexicalStat/data/robespierre.R
@@ -4,15 +4,15 @@ republique <- c(35,   10,  16,  29,  29,   9,  21, 14,  2, 42 );
 ennemi     <- c(30,   13,  11,  19,  22,  10,  16,  7,  2, 35 );
 patrie     <- c(6,     5,  16,   8,  23,  10,  35,  8,  3, 39 );
 
-robespierre <- matrix(c(de, peuple, republique, ennemi, patrie, rep(0, 10)), nrow=6, byrow=TRUE)
+partSize   <- c(8395, 2558, 3920, 6903, 7896, 4555, 10142, 2063, 1084, 13933);
+
+robespierre <- rbind(de, peuple, republique, ennemi, patrie)
+robespierre <- rbind(robespierre, partSize - colSums(robespierre));
+
 tokens <- c("de", "peuple", "republique", "ennemi", "patrie");
 rownames(robespierre) <- c(tokens, "others");
 colnames(robespierre) <- paste("D", 1:10, sep="");
 
-partSize <- c(8395, 2558, 3920, 6903, 7896, 4555, 10142, 2063, 1084, 13933);
-robespierre[6,] <- partSize - colSums(robespierre);
-
 rm(list=c(tokens, "partSize"));
 rm("tokens");
-robespierre <- t(robespierre);
 
