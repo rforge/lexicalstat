cef599ea1c7ede2ab3f4aee722e913bbc351a973
sloiseau <sloiseau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-30 10:10:22 +0000

    testing frequencyList
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/lexicalStat/tests/unitTests/runit.frequencyList.R
@@ -0,0 +1,34 @@
+test_frequencyList_vector2frequencyList <- function() {
+	x <- c(1, 2, 1, 2, 1, 3);
+	names(x) <- LETTERS[1:length(x)];
+	fl <- vector2frequencyList(x);
+}
+
+test_frequencyList_vector2frequencyList_nonames <- function() {
+	x <- c(1, 2, 1, 2, 1, 3);
+	names(x) <- LETTERS[1:length(x)];
+	checkException(vector2frequencyList(x));
+}
+
+test_frequencyList_vector2frequencyList_empty_names <- function() {
+	x <- c(1, 2, 1, 2, 1, 3);
+	names(x) <- c(LETTERS[1:(length(x)-1)], "");
+	checkException(vector2frequencyList(x));
+}
+
+test_frequencyList_vector2frequencyList_not_numeric <- function() {
+	checkException(vector2frequencyList(LETTERS[1:10]));
+}
+
+test_frequencyList_vector2frequencyList2 <- function() {
+	c <- LETTERS[sample(1:20, 20, replace=T)];
+	f <- table(c)
+	fl <- vector2frequencyList(f);
+}
+
+test_frequencyList_print <- function() {
+	c <- LETTERS[sample(1:20, 20, replace=T)];
+	f <- table(c)
+	fl <- vector2frequencyList(f);
+	print(fl);
+}
