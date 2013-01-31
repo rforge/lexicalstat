848baad810da4b35af5d6969041f61423af16d1c
sloiseau <sloiseau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-31 18:51:20 +0000

    moved
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@94 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lexicalStat/inst/exempleData/robespierre.R
+++
@@ -1,5 +0,0 @@
-library(lexicalStat);
-robespierre <- read.table("robespierre.tab")
-rownames(robespierre) <- tolower(rownames(robespierre));
-robespierre <- lexicalTable(robespierre);
-save(robespierre, file="robespierre.rda");
