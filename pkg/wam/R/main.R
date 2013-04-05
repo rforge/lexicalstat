6a34a3085b34b777521501f93bc48e105c059c96
bdesgraupes <bdesgraupes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-05 13:05:31 +0000

    Added wam pkg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@126 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/wam/R/main.R
@@ -0,0 +1,9 @@
+# ===========================================================================
+# File: "main.R"
+#                        Created: 2013-04-05 14:55:29
+#              Last modification: 2013-04-05 14:55:29
+# Author: Bernard Desgraupes
+# e-mail: <bernard.desgraupes@u-paris10.fr>
+# This file is part of the wam project.
+# ===========================================================================
+
---
+++ pkg/wam/R/zzz.R
@@ -0,0 +1,12 @@
+# ===========================================================================
+# File: "zzz.R"
+#                        Created: 2013-04-05 14:55:29
+#              Last modification: 2013-04-05 14:55:29
+# Author: Bernard Desgraupes
+# e-mail: <bernard.desgraupes@u-paris10.fr>
+# This file is part of the wam project.
+# ===========================================================================
+
+# .onLoad <-function (lib, pkg) {
+# 	library.dynam("wam", pkg, lib)
+# }
