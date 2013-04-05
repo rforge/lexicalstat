e0e894cb449875757339650937bcf2cf8294dc0e
bdesgraupes <bdesgraupes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-05 13:34:27 +0000

    Description file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/wam/R/main.R
+++ pkg/wam/R/main.R
@@ -2,8 +2,25 @@
 # File: "main.R"
 #                        Created: 2013-04-05 14:55:29
 #              Last modification: 2013-04-05 14:55:29
-# Author: Bernard Desgraupes
-# e-mail: <bernard.desgraupes@u-paris10.fr>
+# Authors: Bernard Desgraupes <bernard.desgraupes@u-paris10.fr>
+#          Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
 # This file is part of the wam project.
 # ===========================================================================
+
+## 
+ # ------------------------------------------------------------------------
+ # 
+ # "cphyper(x)" --
+ # 
+ # Example:
+ #      > cphyper(q, m, n, k)
+ # 
+ # ------------------------------------------------------------------------
+ ##
+cphyper <- function(q, m, n, k, lower.tail = TRUE, log.p = FALSE) {
+	ans <- .Call("wam_cphyper", q, m, n, k, lower.tail, log.p, PACKAGE="wam")
+    return(ans)
+}
+
+
 
