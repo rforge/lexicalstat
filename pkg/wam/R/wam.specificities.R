753c798d73a441876159473f1fb015333943b645
sloiseau <sloiseau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-05 17:27:02 +0000

    first version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/wam/R/wam.specificities.R
@@ -0,0 +1,51 @@
+# ===========================================================================
+# File: "wam.specificities.R"
+#                        Created: 2013-04-05 14:55:29
+#              Last modification: 2013-04-05 14:55:29
+# Authors: Bernard Desgraupes <bernard.desgraupes@u-paris10.fr>
+#          Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
+# This file is part of the wam project.
+# ===========================================================================
+
+#
+# word association measure defined in : LAFON, Pierre, 1980. « Sur la
+# variabilité de la fréquence des formes dans un corpus », /Mots/, 1, pp.
+# 127--165
+#
+# http://www.persee.fr/web/revues/home/prescript/article/mots_0243-6450_1980_num_1_1_1008
+#
+
+wam.specificities <- function(N, n, K, k, method="base") {
+  len <- max(N, n, K, k);
+  N <- rep(N, length.out=len);
+  n <- rep(n, length.out=len);
+  K <- rep(K, length.out=len);
+  k <- rep(k, length.out=len);
+
+  # the number of black balls in the urn.
+  B <- N-K;
+
+  # the mode
+  mo <- floor((n+1)*(B+1)/(N+2));
+  #mode <- ((recycled$whites + 1) * (recycled$drawn + 1)) / (N + 2);
+
+  if (method == "log") { # use log.p and lower.tail
+    specif <- ifelse(k <= mo, -phyper(k, B, N-B, n, log.p=TRUE), phyper(k, B, N-B, n, log.p=TRUE, lower.tail=TRUE));
+  } else { # compute cdk
+    cdk <- ifelse(k <= mo, -phyper(k, B, N-B, n), 1-phyper(k-1, B, N-B, n));
+    if (method == "base") {
+      specif <- cdk;
+    } else { # compute cdmo (cumulative probability for the mode)
+      cdmo <- phyper(mo, B, N-B, n);
+      if else (method == "gap") {
+	specif <- ifelse(k <= mo, -abs(cdmo-cdk), abs(cdmo-cdk));
+      } if else (method == "logscale") {
+	specif <- ifelse(k <= mo, -abs((cdmo-cdk)/cdk) , abs((cdmo-cdk)/cdk));
+      } if else (method == "scale") {
+	specif <- ifelse(k <= mo, -abs(cdmo-cdk)/cdmo, abs(cdmo-cdk)/cdmo);
+      }
+    }
+  }
+  return(specif);
+}
+
