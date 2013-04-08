baac00e45b1ef2c4634383b08c8df919bc597a6e
bdesgraupes <bdesgraupes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-08 10:01:09 +0000

    Fixes in wam.specificities
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lexicalstat@133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/wam/R/wam.specificities.R
+++ pkg/wam/R/wam.specificities.R
@@ -1,7 +1,7 @@
 # ===========================================================================
 # File: "wam.specificities.R"
 #                        Created: 2013-04-05 14:55:29
-#              Last modification: 2013-04-05 14:55:29
+#              Last modification: 2013-04-08 11:59:45
 # Authors: Bernard Desgraupes <bernard.desgraupes@u-paris10.fr>
 #          Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
 # This file is part of the wam project.
@@ -16,37 +16,30 @@
 #
 
 wam.specificities <- function(N, n, K, k, method="log") {
-  len <- max(length(N), length(n), length(K), length(k));
-  N <- rep(N, length.out=len);
-  n <- rep(n, length.out=len);
-  K <- rep(K, length.out=len);
-  k <- rep(k, length.out=len);
+	# mode
+	mo <- floor((n+1)*(K+1)/(N+2));
+	
+	# cdf (or its log)
+	islog <- (method %in% c("log","logscale"))
+	cdk <- ifelse(k <= mo, phyper(k, K, N-K, n, log.p=islog), phyper(k-1, K, N-K, n, log.p=islog, lower.tail=FALSE));
+	specif <- double(length(k));
+	
+	if (method == "base") {
+		specif <- ifelse(k <= mo, -cdk, cdk);
+	} else { 
+		# cumulative probability for the mode (or its log)
+		cdmo <- phyper(mo, K, N-K, n, log.p=islog);
+		if (method == "gap") {
+			specif <- ifelse(k <= mo, -abs(cdmo-cdk), abs(cdmo-cdk));
+		} else if (method == "log") {
+			specif <- ifelse(k <= mo, -abs(cdmo-cdk), abs(cdmo-cdk));
+		} else if (method == "scale") {
+			specif <- ifelse(k <= mo, -abs(cdmo-cdk)/cdmo, abs(cdmo-cdk)/cdmo);
+		} else if (method == "logscale") {
+			specif <- ifelse(k <= mo, -abs((cdmo-cdk)/cdk) , abs((cdmo-cdk)/cdk));
+		} 
+	}
 
-  # the number of black balls in the urn.
-  B <- N-K;
-
-  # the mode
-  mo <- floor((n+1)*(B+1)/(N+2));
-  #mode <- ((recycled$whites + 1) * (recycled$drawn + 1)) / (N + 2);
-
-  specif <- double(len);
-  if (method == "log") { # use log.p and lower.tail
-    specif <- ifelse(k <= mo, -abs(phyper(k, K, N-K, n, log.p=TRUE)), abs(phyper(k, K, N-K, n, log.p=TRUE, lower.tail=FALSE)));
-  } else { # compute cdk
-    cdk <- ifelse(k <= mo, -phyper(k, K, N-K, n), 1-phyper(k-1, K, N-K, n));
-    if (method == "base") {
-      specif <- cdk;
-    } else { # compute cdmo (cumulative probability for the mode)
-      cdmo <- phyper(mo, B, N-B, n);
-      if (method == "gap") {
-	specif <- ifelse(k <= mo, -abs(cdmo-cdk), abs(cdmo-cdk));
-      } else if (method == "logscale") {
-	specif <- ifelse(k <= mo, -abs((cdmo-cdk)/cdk) , abs((cdmo-cdk)/cdk));
-      } else if (method == "scale") {
-	specif <- ifelse(k <= mo, -abs(cdmo-cdk)/cdmo, abs(cdmo-cdk)/cdmo);
-      }
-    }
-  }
-  return(specif);
+	return(specif);
 }
 
